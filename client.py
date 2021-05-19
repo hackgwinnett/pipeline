a09651eed9332fd07a493ae3e952de0f156a1d60
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-05-18 21:51:54 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,25 @@
+import requests
+
 '''
     raw_arr = 2d array of size [n][2]
     arr[0][0] = field 1, arr[0][1] = value 1
     arr[1][0] = field 2, arr[1][1] = value 2
     arr[2][0] = field 3, arr[2][1] = value 3
+    .....
+    arr[n][0] = field n, arr[n][1] = value n
 '''
-def create_add_args(raw_arr, password):
-    storage = "password:" + str(password)
+def create_add_args(raw_arr, password, filename):
+    storage = "add,"
+    storage += filename + ","
+    storage += "password:" + str(password)
     for i in range(len(raw_arr)):
         if (i < (len(raw_arr))):
-            storage += "_"
+            storage += ","
         storage += str(raw_arr[i][0]) + ":" + str(raw_arr[i][1])
     return storage
+
+# TODO: def create_read_args
+
+def send(address, args):
+    r = requests.get(address + "/" + args)
+    return r.text
