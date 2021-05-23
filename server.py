b3e9d3adb336983b7c05b6a5f45fb81bf924e9b6
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-05-23 11:28:06 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -36,18 +37,48 @@ def parse(raw):
 
     # add command:
     if args[0] == "add":
+
         filename = args[1].split(".")[0]
+        if os.path.exists(filename):
+            return "file: " + filename + " already exists"
+        
         f = open(filename, "a")
         filecontent = ""
         i = 2
         while i < len(args):
-            filecontent += args[i] + " "
+            filecontent += args[i]
+            if i < len(args) - 1:
+                filecontent += ","
             i+=1
         f.write(filecontent)
         f.close()
 
-    # send message back to the client (flask throws errors if file creation is not successful)
-    return "data successfully stored"
+        # send message back to the client (flask handles errors if file creation is not successful)
+        return "data successfully stored"
+
+
+    # read command:
+    if args[0] == "read":
+        
+        filename = args[1].split(".")[0]
+        user_password = args[2].split(":")[1]
+        file_password = ""
+        
+        if os.path.exists(filename):
+            f = open(filename, "r")
+            content = f.read()
+            raw_add = content.split(",")
+            for field in raw_add:
+                if "password:" in field:
+                    file_password = field.split(":")[1]
+            if user_password == file_password:
+                return content
+            else:
+                return "password is invalid: " + user_password
+
+        else:
+            return "file does not exist: " + filename
+
 
 if __name__ == "__main__":
     app.run()
