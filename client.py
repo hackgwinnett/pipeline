a703b493b04f6a9d9b759dd760505edc3d1b91d2
Garrett Rector <garrett@grector.dev>
2021-08-15 19:50:52 -0400

    Added reading/writing to directories

--- client.py
+++ client.py
@@ -8,22 +8,42 @@ import requests
     .....
     arr[n][0] = field n, arr[n][1] = value n
 '''
-def create_add_args(raw_arr, password, filename):
+
+
+def create_add_args(raw_arr, filename, password=None):
+    filename = str(filename).replace("/", ">")
     storage = "add,"
     storage += filename + ","
     storage += "password:" + str(password)
     for i in range(len(raw_arr)):
-        if (i < (len(raw_arr))):
+        if i < (len(raw_arr)):
             storage += ","
         storage += str(raw_arr[i][0]) + ":" + str(raw_arr[i][1])
     return storage
 
-def create_read_args(filename, password):
+
+def create_dir_args(dirname, password=None):
+    dirname = str(dirname).replace("/", ">")
+    storage = "add_dir,"
+    storage += dirname + ","
+    storage += "password:" + str(password)
+    return storage
+
+
+def create_read_args(filename, password=None):
+    filename = str(filename).replace("/", ">")
     return "read," + str(filename) + ",password:" + password
+
+
+def create_read_dir_args(dirname, password=None):
+    dirname = dirname = str(dirname).replace("/", ">")
+    return "read_dir," + str(dirname) + ",password:" + str(password)
+
 
 def send(address, args):
     r = requests.get(address + "/" + args)
     return r.text
+
 
 def parse(raw, field):
     arr = raw.split(",")
