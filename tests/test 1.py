3ffa15543570c9c3a361356b2300671f28da03d1
Garrett Rector <garrett@grector.dev>
2021-08-15 19:51:32 -0400

    Added argument order standardization

---
+++ tests/test 1.py
@@ -0,0 +1,45 @@
+import client
+
+
+def main():
+    # set address (localhost address shown here for debugging server):
+    address = "http://127.0.0.1:5000"
+
+    # set file contents in a 2D array by appending 1D arrays to a list:
+    file1content = []
+    field1 = ["field1", "file 1 content 1"]
+    field2 = ["field2", "file 1 content 2"]
+    field3 = ["field3", "file 1 content 3"]
+    file1content.append(field1)
+    file1content.append(field2)
+    file1content.append(field3)
+
+    file2content = []
+    _field1 = ["field1", "file 2_content 1"]
+    _field2 = ["field2", "file 2 content 2"]
+    _field3 = ["field3", "file 2 content 3"]
+    file2content.append(_field1)
+    file2content.append(_field2)
+    file2content.append(_field3)
+
+    # create the files on the server side:
+    createfile1 = client.create_add_args(file1content, "file_1", "test_password_1")
+    createfile2 = client.create_add_args(file2content, "file_2", "test_password_2")
+    client.send(address, createfile1)
+    client.send(address, createfile1)
+
+    # test server side password verification:
+    correct_args = client.create_read_args("file_1", "test_password_1")
+    faulty_args = client.create_read_args("file_1", "wrong_pass")
+    print(client.send(address, correct_args))
+    print(client.send(address, faulty_args))
+
+    # get raw content and read specific fields:
+    content = client.send(address, correct_args)
+    print(client.parse(content, "field1"))
+    print(client.parse(content, "field2"))
+    print(client.parse(content, "field3"))
+
+
+if __name__ == '__main__':
+    main()
